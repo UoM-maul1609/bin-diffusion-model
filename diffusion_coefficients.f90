dcdc46c438262092acf83c94b48c30c1ade2775a
kathrynfowler91 <kathryn.fowler@manchester.ac.uk>
2018-06-12 17:26:31 +0100

    update to include makefile

--- diffusion_coefficients.f90
+++ diffusion_coefficients.f90
@@ -1,28 +1,36 @@
 module diffusion_coefficients
 
+
+private
+public :: read_in_dc_namelist, diffusion_coefficient
+
 contains
 
-! read_in_namelist
-subroutine read_in_dc_namelist(nmlfile, kp, n_comp, molefrac, t, d_s, param, compound, d_c)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! read_in_namelist							       !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine read_in_dc_namelist(nmlfile, kp, n_comp, molefrac, t, d_self, param, compound, d_coeff)
 	implicit none
 	integer, intent(out) :: kp, n_comp
 	real, intent(out) :: t
 	real, allocatable, dimension(:), intent(out) :: molefrac
-	real, allocatable, dimension (:), intent(out) :: d_c
-	real, allocatable, dimension (:), intent(out) :: d_s
+	real, allocatable, dimension (:), intent(out) :: d_coeff
+	real, allocatable, dimension (:), intent(out) :: d_self
 	character (len=50), intent(out) :: param, compound
 	character (len=11), intent(in) :: nmlfile
 
 	! define namelists
 	namelist /dc_setup/ kp, n_comp
-	namelist /dc_vars/ molefrac, t, d_s, param, compound
+	namelist /dc_vars/ molefrac, t, d_self, param, compound
 
 	! read in namelist
 	open(10, file=nmlfile, status='old', recl=80, delim='apostrophe')
 	read(10, nml=dc_setup)			
 		allocate (molefrac(1:kp))
-		allocate (d_s(1:n_comp))
-		allocate (d_c(1:kp))
+		allocate (d_self(1:n_comp))
+		allocate (d_coeff(1:kp))
 	read(10, nml=dc_vars)	
 
 	close(10)
@@ -30,57 +38,64 @@ subroutine read_in_dc_namelist(nmlfile, kp, n_comp, molefrac, t, d_s, param, com
 end subroutine read_in_dc_namelist
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! d_coeff
-subroutine d_coeff(kp, molefrac, t, d_s, param, compound, d_c)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! diffusion_coefficient							       !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine diffusion_coefficient(kp, molefrac, t, d_self, param, compound, d_coeff)
 	implicit none
 	integer, intent(in) :: kp
 	real, intent(in) :: t
 	real, allocatable, dimension(:), intent(in) :: molefrac
-	real, allocatable, dimension (:), intent(inout) :: d_c
-	real, allocatable, dimension (:), intent(in) :: d_s
+	real, allocatable, dimension (:), intent(inout) :: d_coeff
+	real, allocatable, dimension (:), intent(in) :: d_self
 	character (len=50), intent(in) :: param, compound
 
 	select case (param)
 	case ('constant')
 	! constant diffusion coefficient
-		d_c(:)=d_s(1)
+		d_coeff(:)=d_self(1)
 	case ('darken')
 	! linear relation between water molefraction and diffusion coefficient
-		d_c=d_s(1)*(1-molefrac)+d_s(2)*(molefrac)
+		d_coeff=d_self(1)*(1-molefrac)+d_self(2)*(molefrac)
 	case ('vignes')
 	! logarithmic relation between water mole fraction and diffusion coefficient
-		d_c=d_s(1)**(1-molefrac)*d_s(2)**(molefrac)
+		d_coeff=d_self(1)**(1-molefrac)*d_self(2)**(molefrac)
 	case('Lienhard2014')
 	! http://pubs.rsc.org/en/content/articlehtml/2014/cp/c4cp01939c
-		call Lienhard2014(t, molefrac, compound, d_c)
+		call Lienhard2014(kp, t, molefrac, compound, d_coeff)
 	case('Lienhard2015')
 	! http://www.atmos-chem-phys-discuss.net/15/24473/2015/acpd-15-24473-2015.pdf [discussion]
 	! http://www.atmos-chem-phys.net/15/13599/2015/acp-15-13599-2015.pdf [final paper]
-		call Lienhard2015(t, molefrac, compound, d_c)
+		call Lienhard2015(kp, t, molefrac, compound, d_coeff)
 	case default
 		print*, "selected param not found"
 	end select
 
-	print*, "diffusion coefficient = ", d_c
+	print*, "diffusion coefficient = ", d_coeff
 
-end subroutine d_coeff
+end subroutine diffusion_coefficient
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Lienhard2014
-! http://pubs.rsc.org/en/content/articlehtml/2014/cp/c4cp01939c
-subroutine Lienhard2014(t, molefrac, compound, d_c)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Lienhard2014								       !
+! http://pubs.rsc.org/en/content/articlehtml/2014/cp/c4cp01939c 	       !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine Lienhard2014(kp, t, molefrac, compound, d_coeff)
 	implicit none
+	integer, intent(in) :: kp
 	real, intent(in) :: t
 	real, allocatable, dimension(:), intent(in) :: molefrac
 	character (len=50), intent(in) :: compound
-	real, allocatable, dimension (:), intent(inout) :: d_c
-	real :: d_cit, d_w, c, d
+	real, allocatable, dimension (:), intent(inout) :: d_coeff
+	real :: d_coeffit, d_w, c, d
 	real, allocatable, dimension(:) :: alpha
 
+	allocate (alpha(1:kp))
+
 	if (compound == 'citric acid') then
-		d_cit = 10.**(-15.-(175./(t-208.)))
+		d_coeffit = 10.**(-15.-(175./(t-208.)))
 		d_w = 10.**(-6.514-(387.4/(t-118.)))
 		if (t>265) then
 			c = -41.+0.143*265.
@@ -93,7 +108,7 @@ subroutine Lienhard2014(t, molefrac, compound, d_c)
         		d = -69.+0.28*t
    		end if
 		alpha = exp((1.-molefrac)**2.*(c+3.*d-4.*d*(1.-molefrac)))
-    		d_c = d_w**(molefrac*alpha)*d_cit**(1.-molefrac*alpha)
+    		d_coeff = d_w**(molefrac*alpha)*d_coeffit**(1.-molefrac*alpha)
 	else
 		print*, 'selected compound not found'
 	end if
@@ -101,20 +116,28 @@ subroutine Lienhard2014(t, molefrac, compound, d_c)
 end subroutine Lienhard2014
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Lienhard2015
-! http://www.atmos-chem-phys-discuss.net/15/24473/2015/acpd-15-24473-2015.pdf [discussion]
-! http://www.atmos-chem-phys.net/15/13599/2015/acp-15-13599-2015.pdf [final paper]
-subroutine Lienhard2015(t, molefrac, compound, d_c)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Lienhard2015								       !	
+! http://www.atmos-chem-phys-discuss.net/15/24473/2015/acpd-15-24473-2015.pdf  !
+! [discussion]								       !
+! http://www.atmos-chem-phys.net/15/13599/2015/acp-15-13599-2015.pdf 	       !
+! [final paper]								       !
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine Lienhard2015(kp, t, molefrac, compound, d_coeff)
 	implicit none
+	integer, intent(in) :: kp
 	real, intent(in) :: t
 	real, allocatable, dimension(:), intent(in) :: molefrac
 	character (len=50), intent(in) :: compound
-	real, allocatable, dimension (:), intent(inout) :: d_c
+	real, allocatable, dimension (:), intent(inout) :: d_coeff
 	real :: logdwtg0, eact, tg, a, a1, a2, b, b1, b2, &
 		t0, t1, t2, ta, tb, s, zeta_a_0_aw0, zeta_a_aw0, &
 		zeta_v_0, zeta_v, dwt0
 	real, allocatable, dimension(:) :: alpha, dwt1
+
+	allocate (alpha(1:kp), dwt1(1:kp))
+	
 
 	select case (compound)
 	case ('levoglucosan')
@@ -217,7 +240,7 @@ subroutine Lienhard2015(t, molefrac, compound, d_c)
 	
 	!alpha=exp( (1-molefrac).^2) 
 	!alpha=1.
-	alpha=exp( (1.-molefrac)**2.*(a+3.*b-4.*b*(1-molefrac))  )
+	alpha(:)=exp( (1.-molefrac)**2.*(a+3.*b-4.*b*(1-molefrac))  )
 
 	t0=118
 	s=892
@@ -228,39 +251,12 @@ subroutine Lienhard2015(t, molefrac, compound, d_c)
 	dwt1=exp(zeta_a_aw0+(molefrac)*alpha*(zeta_v-zeta_a_aw0))
 	
 	! cm**2 s**-1
-	d_c=dwt0**(1-molefrac*alpha)*dwt1**(molefrac*alpha)
+	d_coeff=dwt0**(1-molefrac*alpha)*dwt1**(molefrac*alpha)
 	! m**2 s**-1
-	d_c=d_c*1.e-4
+	d_coeff=d_coeff*1.e-4
 
 end subroutine Lienhard2015
 
 end module diffusion_coefficients
 
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-program main
-
-use diffusion_coefficients
-implicit none
-
-integer :: kp, n_comp
-real :: t 
-real, allocatable, dimension(:) :: molefrac
-real, allocatable, dimension (:) :: d_s
-real, allocatable, dimension (:) :: d_c
-character (len=50) :: param, compound
-
-
-call read_in_dc_namelist('namelist.in', kp, n_comp, molefrac, t, d_s, param, compound, d_c)
-
-
-call d_coeff(kp, molefrac, t, d_s, param, compound, d_c)
-
-
-end program main
-
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
---
+++ main.f90
@@ -0,0 +1,23 @@
+program main
+
+use diffusion_coefficients
+
+implicit none
+
+integer :: kp, n_comp
+real :: t 
+real, allocatable, dimension(:) :: molefrac
+real, allocatable, dimension (:) :: d_self
+real, allocatable, dimension (:) :: d_coeff
+character (len=50) :: param, compound
+
+
+! read in namelists
+call read_in_dc_namelist('namelist.in', kp, n_comp, molefrac, t, d_self, param, compound, d_coeff)
+
+
+! calculate and print diffusion coefficients
+call diffusion_coefficient(kp, molefrac, t, d_self, param, compound, d_coeff)
+
+
+end program main
