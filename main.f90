3227baeafcff4e25d8e0f28e54b1012d8e2798d1
kathrynfowler91 <kathryn.fowler@manchester.ac.uk>
2018-06-20 17:57:23 +0100

    change namelist param and compound to integers

--- main.f90
+++ main.f90
@@ -4,12 +4,11 @@ use diffusion_coefficients
 
 implicit none
 
-integer :: kp, n_comp
+integer :: kp, n_comp, i, param, compound
 real :: t 
 real, allocatable, dimension(:) :: molefrac
 real, allocatable, dimension (:) :: d_self
 real, allocatable, dimension (:) :: d_coeff
-character (len=20) :: param, compound
 
 
 ! read in namelists
@@ -18,6 +17,12 @@ call read_in_dc_namelist('namelist.in', kp, n_comp, molefrac, t, d_self, param, 
 
 ! calculate and print diffusion coefficients
 call diffusion_coefficient(kp, molefrac, t, d_self, param, compound, d_coeff)
+
+print*, 'molefrac', ':', 'd_coeff'
+	do i=1,kp
+		write(*,10) molefrac(i), ' : ', d_coeff(i)
+	end do
+10	format (f4.2,a,e9.3)
 
 
 end program main
